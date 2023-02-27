795191e040c592b91767d291f5d73437fbbc4f69
Alexandr <golubovskiy.al@mail.ru>
2023-02-27 18:33:15 +0300

    feat: add solution for extra task1 JS Intro

---
+++ extra tasks/JS Intro/task1.js
@@ -0,0 +1,15 @@
+const findMaxSum = (array) => {
+  let maxSum = Math.max.apply(null, array);
+
+  for (let i = 0; i < array.length; i++) {
+    for (let k = i; k < array.length; k++) {
+      if (array.slice(i, k + 1).reduce((acc, el) => acc + el, 0) > maxSum) {
+        maxSum = array.slice(i, k + 1).reduce((acc, el) => acc + el, 0);
+      }
+    }
+  }
+
+  return maxSum;
+};
+
+console.log(findMaxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
